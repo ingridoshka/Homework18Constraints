48b958d0842490cac7b9dcba94ac81cf881434e5
Ingridoshka <Ingridoshka@MacBooks-MacBook-Pro.local>
2017-05-08 10:12:52 +0300

    Initial Commit

---
+++ Homework18Constrains/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework18Constrains
+//
+//  Created by Ingridoshka on 2017-05-06.
+//  Copyright © 2017 MacBook Retina 2014 256Gb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
